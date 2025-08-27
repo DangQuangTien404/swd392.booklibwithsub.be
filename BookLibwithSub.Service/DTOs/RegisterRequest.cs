53de5c5ee2ecb44be4a4895d41ac3673827f0b9a
DangQuangTien <dangquangtien16102002@gmail.com>
2025-08-27 19:37:08 +0700

    Revert "regis control errormess"
    This reverts commit 8f6bbc8c757cf2714f26f15492c30651032c91c4.

--- BookLibwithSub.Service/DTOs/RegisterRequest.cs
+++ BookLibwithSub.Service/DTOs/RegisterRequest.cs
@@ -1,31 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using BookLibwithSub.Service.Constants;
 
 namespace BookLibwithSub.Service.Models
 {
     public class RegisterRequest
     {
-        [Required(ErrorMessage = "Username là bắt buộc.")]
-        [RegularExpression(@"^(?![0-9])[a-zA-Z0-9_]{3,20}$",
-            ErrorMessage = "Username phải từ 3-20 ký tự, không bắt đầu bằng số, chỉ chứa chữ/số/_")]
+        [Required]
         public string Username { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Password là bắt buộc.")]
-        //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$",
-        //    ErrorMessage = "Password tối thiểu 6 ký tự, có chữ hoa, chữ thường, số và ký tự đặc biệt.")]
+        [Required]
         public string Password { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Họ tên là bắt buộc.")]
-        [MinLength(3, ErrorMessage = "Họ tên phải ít nhất 3 ký tự.")]
+        [Required]
         public string FullName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Email là bắt buộc.")]
-        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Số điện thoại là bắt buộc.")]
-        [RegularExpression(@"^(\+84|0)\d{8,10}$",
-            ErrorMessage = "Số điện thoại phải từ 9-11 số, bắt đầu bằng 0 hoặc +84.")]
+        [Phone]
         public string PhoneNumber { get; set; } = string.Empty;
 
         public string Role { get; set; } = Roles.User;
